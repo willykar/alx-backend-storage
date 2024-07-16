4122d97d41d5751df04a5a1eea4aa50d6f5ffa74
willykar <wilsonnjenga799@gmail.com>
2024-07-16 14:00:58 -0700

    schools_by_topic

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -7,6 +7,5 @@ def schools_by_topic(mongo_collection, topic):
     """ A function that returns the list of school having
     a specific topic"""
     filter_topics = mongo_collection.find(
-            {"topics": topic}
-)
+            {"topics": topic})
     return [document for document in filter_topics]
