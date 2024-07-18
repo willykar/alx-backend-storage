ce6732f4cf2c67f1fb13b652326c735a194690e3
willykar <103494595+willykar@users.noreply.github.com>
2024-07-18 06:05:38 +0300

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,70 +2,37 @@
 """ Web module that uses the requests module to obtain the
     HTML content of a particular URL and returns it
 """
-import requests
-import redis
-from functools import wraps
-from typing import Callable
-import time
-
-r = redis.Redis()
-r.flushdb()
 
 
-def cache_count(method: Callable) -> Callable:
-    """ A Cache Count Decorator function """
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
+
+
+def data_cacher(method: Callable) -> Callable:
+    '''A data_cacher function that caches
+       the output of fetched data
+    '''
     @wraps(method)
-    def wrapper(url: str) -> str:
-        """ Wrapper Function """
-        count_key = f"count:{url}"
-        cached_url = f"cached:{url}"
-        cached = r.get(cached_url)
-
-        if cached:
-            r.incr(count_key)
-            return (cached.decode('utf-8'))
-
-        content = method(url)
-        r.setex(cached_url, 10, content)
-        r.set(count_key, 1)
-        return (content)
-
-    return (wrapper)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
 
 
-@cache_count
+@data_cacher
 def get_page(url: str) -> str:
-    """ A get_Page Function """
-    try:
-        res = requests.get(url).text
-        return (res)
-    except requests.RequestException as e:
-        return ("")
-
-
-if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk"
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    get_page(url)
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
-
-    time.sleep(5)
-    print("After 5")
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
-    time.sleep(4)
-    print("After 9")
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
-    time.sleep(1)
-    print("After 10")
-    print(r.get(f"count:{url}"))
-    print(r.get(f"cached:{url}"))
+    '''
+    A get_page function
+    Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
